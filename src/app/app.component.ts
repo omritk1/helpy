9f740510206607a89ea1f12de177911d077e15e0
tcwd <galili.omri@gmail.com>
2017-07-28 12:52:02 +0300

    adding helpy first ng

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Http} from '@angular/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  public user= {
+    name:'',
+    password: ""
+  };
+
+// postToServer(){
+//
+//   console.log(this.user);
+// }
+
+constructor(private x:Http){
+  this.x.post('nativ', this.user).subscribe()
+}
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule }   from '@angular/forms';
+import { HttpModule }    from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { WorldComponent } from './world/world.component';
+import { NavComponent } from './nav/nav.component';
+import { FormComponent } from './form/form.component';
+import { FrontComponent } from './front/front.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WorldComponent,
+    NavComponent,
+    FormComponent,
+    FrontComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
